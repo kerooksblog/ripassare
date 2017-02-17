c433654d7be5242474125f98d041152bbe9c5994
kerooksblog <kerooksblog@users.noreply.github.com>
2017-02-17 17:18:32 +0100

    Create boneup.py
    This program ask you random question from a txt you've created before. 
    In this way you can bone up for you're exam ;)

---
+++ boneup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# boneup.py
+#
+# Copyright 2017 kerook <crafton.anna@gmail.com>
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#    
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#     GNU General Public License for more details.
+#    
+#     You should have received a copy of the GNU General Public License
+#     along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Please note:
+# You should replace the file name with the one you're interesting in. (Example: here it's "questions")
+# The file questions.txt must be in the same folder of this program.
+# The file questions.txt must have ONE question per line.
+
+""" This program ask you random question from a txt you've created before. 
+In this way you can bone up for you're exam ;)"""
+
+import random
+print(random.choice(list(open("./questions.txt","r"))))
